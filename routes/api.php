97d2dfa54ea24d237ab01b2ad6667d8be33fc378
Binura Yasantha <binurayasantha@gmail.com>
2021-04-03 10:49:29 +0530

    changes of api.php

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/register',function(Request $request){
     // $user = User::where([['username'=> $request->username],['psd'=> $request->psd]])->get();
     // return $user;
-    // "select * from user where username=dgcyhscuj and psd=gcvdayhcuj"
+    return "select * from user where username=dgcyhscuj and psd=gcvdayhcuj";
     return [
         "data" => [
             "username" => "cvgdahcvsdujvsujv",
